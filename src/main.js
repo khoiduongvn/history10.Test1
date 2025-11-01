b04d40880793b2ced2fc1609b64c25f34ad4ab08
chinh.dq <chinhduongquoc@gmail.com>
2025-11-01 19:28:41 +0700

    delete

--- src/main.js
+++ src/main.js
@@ -1,11 +1,3 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 import {createBootstrap} from 'bootstrap-vue-next/plugins/createBootstrap'
-
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-createApp(App)
-    .use(router)
-    .use(createBootstrap())
-    .mount('#app')
