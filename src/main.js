f3441fa964e651bd6f37d92c63bd640d0f8f6470
chinh.dq <chinhduongquoc@gmail.com>
2025-11-01 19:42:35 +0700

    update

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 import {createBootstrap} from 'bootstrap-vue-next/plugins/createBootstrap'
 
+//aaaaaaaa
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
